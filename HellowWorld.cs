e24dbf44470dc0afd34d1ce8136f89e697445017
romavoloviov <119790047+romavoloviov@users.noreply.github.com>
2022-12-04 14:28:01 +0300

    Add files via upload

---
+++ HellowWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellowWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //Вывод в консоль приветсвия миру 
+     print("lol");
+     print("HellowWorld");
+     print (2000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
